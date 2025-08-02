9c20ee14f909e4f92ff5fec64dac51eb44ad52bf
Suppy <suppy@globalfoods.com>
2025-08-02 19:36:55 +0300

    Fix: Render port config for Flask

--- dashaboard.py
+++ dashaboard.py
@@ -29,7 +29,6 @@ def download_file(filename):
     return send_from_directory('logs', filename, as_attachment=True)
 
 if __name__ == '__main__':
-import os
-
-port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
