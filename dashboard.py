dea43c61f4f187d462baf4a39ffcb44322edc5d1
Suppy <suppy@globalfoods.com>
2025-08-02 19:39:56 +0300

    Fix deploy error

--- dashboard.py
+++ dashboard.py
@@ -29,6 +29,5 @@ def download_file(filename):
     return send_from_directory('logs', filename, as_attachment=True)
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host='0.0.0.0', port=port)
